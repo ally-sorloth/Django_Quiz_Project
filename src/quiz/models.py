1701cde56e7fad581be642322f8ef6da58972297
ally-sorloth <allysorloth@gmail.com>
2021-01-14 21:48:33 +0300

    create quiz project and models

---
+++ src/quiz/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100),
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+class Quiz(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Quiz Title")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = "Quizzes"
+        
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300, verbose_name="question")
+    
+
+# Create your models here.
