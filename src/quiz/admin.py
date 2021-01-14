912cb5ac012f68dbaffdfc9c05af5c43082d3d5e
ally-sorloth <allysorloth@gmail.com>
2021-01-14 22:19:21 +0300

    create models and add admin

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from quiz.models import Answer, Category, Question, Quiz
+from .models import Category, Quiz, Question, Answer
+
+admin.site.register(Category)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+
 # Register your models here.
