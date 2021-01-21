bd9f2de63a50b202b0b2231f40a1fc8d82e30792
ally-sorloth <allysorloth@gmail.com>
2021-01-21 22:51:36 +0300

    add pagination

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Category, Question, Quiz
 from .serializers import CategorySerializer, CategoryDetailSerializer, QuestionSerializer
+from .pagination import MyPagination
 
 
 class CategoryList(generics.ListAPIView):
@@ -21,7 +22,7 @@ class CategoryDetail(generics.ListAPIView):
     
 class QuizDetail(generics.ListAPIView):
     serializer_class = QuestionSerializer
-    
+    pagination_class = MyPagination
     def get_queryset(self):
         queryset = Question.objects.all()
         title = self.kwargs["title"]
