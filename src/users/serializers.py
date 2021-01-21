99dd5cc5a09f38afbc8a956b6658559ba77a9c29
ally-sorloth <allysorloth@gmail.com>
2021-01-21 23:54:04 +0300

    add user register serializer

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,3 +1,4 @@
+from django.db import models
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator 
@@ -5,7 +6,12 @@ from django.contrib.auth.password_validation import validate_password
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(required=True, )
+    email = serializers.EmailField(required=True, validators=[
+                                   UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(write_only=True, required=True, validators=[
+                                     validate_password], style={'input_type': 'password'})
+    password2 = serializers.CharField(write_only=True, required=True, validators=[
+                                      validate_password], style={'input_type': 'password'})
     class Meta:
         model = User
         fields = (
@@ -14,3 +20,21 @@ class RegisterSerializer(serializers.ModelSerializer):
             "password",
             "password2"
         )
+        
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password didn't match"}
+            )
+        return attrs
+    
+    def create(serlf, validated_data):
+        user = User.objects.create(
+            username=validated_data["username"],
+            email=validated_data["email"]
+        )
+        
+        user.set_password(validated_data["password"])
+        user.save()
+        
+        return user
